67ca3247a7e74b0d608935eb677c0cffdf3d3fdb
hoangtuzami <hoangtuzami@gmail.com>
2026-02-13 13:05:14 +0700

    Update swagger

--- src/main/java/com/isums/apigateway/configurations/SecurityConfig.java
+++ src/main/java/com/isums/apigateway/configurations/SecurityConfig.java
@@ -21,10 +21,19 @@ public class SecurityConfig {
                                 "/swagger/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**",
-                                "/api/econtracts/v3/api-docs/**"
-                        ).permitAll()
 
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+
+                                "/api/econtracts/v3/api-docs",
+                                "/api/econtracts/v3/api-docs/**",
+
+                                "/api/houses/v3/api-docs",
+                                "/api/houses/v3/api-docs/**",
+
+                                "/api/asset/v3/api-docs",
+                                "/api/asset/v3/api-docs/**"
+                        ).permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
