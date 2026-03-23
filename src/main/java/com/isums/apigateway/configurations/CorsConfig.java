ba177f89798d9d0a6362c69c44e5259f8bb7bfbd
hoangtuzami <hoangtuzami@gmail.com>
2026-03-23 19:22:08 +0700

    Refactor CorsConfig to replace CorsWebFilter with CorsConfigurationSource for simplification

--- src/main/java/com/isums/apigateway/configurations/CorsConfig.java
+++ src/main/java/com/isums/apigateway/configurations/CorsConfig.java
@@ -3,14 +3,14 @@ package com.isums.apigateway.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsWebFilter corsWebFilter() {
+    public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOriginPattern("http://localhost:5173");
         config.addAllowedOriginPattern("https://localhost:5173");
@@ -30,6 +30,6 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsWebFilter(source);
+        return source;
     }
 }
